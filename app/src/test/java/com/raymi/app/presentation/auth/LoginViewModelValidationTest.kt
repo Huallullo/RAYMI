d31cfcc83f69d744c5ecfcc317e92c1f9796506b
Abel Huallullo <abelhuallullo@gmail.com>
2026-04-26 01:17:50 -0500

    Cambios en TEST

--- app/src/test/java/com/raymi/app/presentation/auth/LoginViewModelValidationTest.kt
+++ app/src/test/java/com/raymi/app/presentation/auth/LoginViewModelValidationTest.kt
@@ -62,5 +62,6 @@ class LoginViewModelValidationTest {
         val state = viewModel.uiState.value
         assertFalse(state.isLoginSuccessful)
         assertEquals("Credenciales inválidas", state.error)
+        dasdasdasdas
     }
 }
